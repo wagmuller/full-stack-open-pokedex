0da30805a51b298e12843c16dcd01ab662041f51
wagmuller <62807925+wagmuller@users.noreply.github.com>
2022-01-07 14:52:19 -0300

    Update todo.spec.js

--- cypress/integration/1-cypress-testing/todo.spec.js
+++ cypress/integration/1-cypress-testing/todo.spec.js
@@ -1,13 +1,13 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.visit('http://localhost:5000') //eslint-disable-line
+    cy.contains('ivysaur')            //eslint-disable-line
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.') //eslint-disable-line
   })
   
   it('fpokemon page can be navigated to', function () {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
+    cy.visit('http://localhost:5000') //eslint-disable-line
+    cy.contains('ivysaur').click().   //eslint-disable-line
+    cy.contains('chlorophyll')        //eslint-disable-line
   })
 })
