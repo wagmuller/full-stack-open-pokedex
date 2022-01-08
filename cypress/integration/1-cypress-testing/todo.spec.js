33f3dc0572948cdd9698e0df9d74686d13811b54
wagmuller <62807925+wagmuller@users.noreply.github.com>
2022-01-08 11:53:27 -0300

    Update todo.spec.js

--- cypress/integration/1-cypress-testing/todo.spec.js
+++ cypress/integration/1-cypress-testing/todo.spec.js
@@ -6,7 +6,7 @@ describe('Pokedex', function() {
   })
   it('fpokemon page can be navigated to', function () {
     cy.visit('http://localhost:5000')
-    cy.contains('ivysaur').click().
-    cy.contains('chlorophyll') //eslint-disable-line
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll') 
   })
 })
